708e3ed2006a26147a9ce1d509c5a65cd19f4533
MorganeLM <lets.morganise.it@gmail.com>
2024-01-29 10:21:52 +0100

    url supabase

--- form.js
+++ form.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from './node_modules/@supabase/supabase-js';
 
 const supabaseUrl = 'https://xhfbydwixbaamsgmbfld.supabase.co';
 const supabaseKey = process.env.SUPABASE_KEY;
